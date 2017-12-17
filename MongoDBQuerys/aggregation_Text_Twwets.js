581e267c2543591ff3c6a1b29506a6cfd3f383b6
jonatan lopez <jonathan.lp17@gmail.com>
2017-12-17 20:07:25 +0100

    move app to folder app and add dockerfile

--- MongoDBQuerys/aggregation_Text_Twwets.js
+++ MongoDBQuerys/aggregation_Text_Twwets.js
@@ -3,22 +3,23 @@ db.tweets.aggregate(
 	// Pipeline
 	[
 		// Stage 1
-		//{
-			//$limit: 1000
-		//},
+		{ $limit: 10},
 
 		// Stage 2
 		{
 			$project: {
 			    "_id" : 1,
 			  	text: 1,
-				user : "$user.screen_name"
+				user : "$user.screen_name",
+				created_at : "$created_at",
+				timestamp_ms : "$timestamp_ms",
+				"@timestamp" : "$timestamp"
 			}
 		},
 
 		// Stage 3
 		{
-			$out: "text_Tweets"
+			$out: "tweetbasics"
 		},
 
 	]
--- docker/api/app/index.js
+++ docker/api/app/index.js
@@ -8,16 +8,23 @@ const restify = require('express-restify-mongoose')
 const app = express()
 const router = express.Router()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json())
 app.use(methodOverride())
+ 
 
-mongoose.connect('mongodb://192.168.99.100:27017/Twitter')
+mongoose.connect('mongodb://Tracking-mongo/Twitter')
 // schema Full Tweet
 var Tweet = new mongoose.Schema({any: [mongoose.Schema.Types.Mixed] }, {"strict": false })
 restify.serve(router, mongoose.model('tweets', Tweet ));
 
 // schema Test_Tweets
-var TextTweet = new mongoose.Schema({ text:String, user: String})
+var TextTweet = new mongoose.Schema({ text:String, user: String}, created_at: String, timestamp_ms: String)
 // no se aceptan mayusculas en el nombre de las colecciones
 // el nombre de las colleciones son plurales, name{s}, vegetale{s}
 restify.serve(router, mongoose.model('tweetbasics', TextTweet ));
